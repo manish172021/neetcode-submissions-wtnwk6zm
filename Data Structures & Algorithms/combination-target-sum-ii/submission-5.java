095f5faca7547d95fac34bbe1a6939ef37ce31b9
Manish Sharma <53977826+manish172021@users.noreply.github.com>
2026-05-11 13:31:11 +0530

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.java
@@ -0,0 +1,66 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        // Arrays.sort(candidates);
+        // Set<List<Integer>> ans = new HashSet<>();
+        // combinationSum2(candidates, target, 0, new ArrayList<>(), ans);
+        // return new ArrayList<>(ans);
+
+        Arrays.sort(candidates);
+        List<List<Integer>> ans = new ArrayList<>();
+        combinationSum2(candidates, target, 0, new ArrayList<>(), ans);
+        return ans;
+
+    }
+
+    // O(2^n⋅k)
+    // 2^n → every element has take / not-take possibility
+    // k → average size of valid combination copied into answer
+    private void combinationSum2(int[] nums, int target, int idx, List<Integer> outputSoFar, List<List<Integer>> ans) {
+        if(target == 0) { // target wala test positive
+            ans.add(new ArrayList<>(outputSoFar));
+            return;
+        }
+        if(target < 0) { // target wala test nedative
+            return;
+        }
+        for(int i = idx; i < nums.length; i++) {
+            int num = nums[i];
+
+            // skip duplicate that can be made from after
+            if(i > idx && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            if(num > target) {
+                break;
+            }
+
+            outputSoFar.add(num);
+            combinationSum2(nums, target - num, i + 1, outputSoFar, ans);
+            outputSoFar.remove(outputSoFar.size() - 1);
+        }
+    }
+
+
+
+    // O(2^n⋅klogM)
+    // 2^n → all subsets generated
+    // k → average size of combination copied into set/list
+    // M → number of elements in HashSet (hash/equality comparisons)
+    private void combinationSum2(int[] nums, int target, int idx, List<Integer> outputSoFar, Set<List<Integer>> ans) {
+        if(target == 0) { // target wala test positive
+            ans.add(new ArrayList<>(outputSoFar));
+            return;
+        }
+        if(target < 0) { // target wala test nedative
+            return;
+        }
+        if(idx >= nums.length) { // array wala test
+            return;
+        }
+        combinationSum2(nums, target, idx + 1, outputSoFar, ans);
+        outputSoFar.add(nums[idx]);
+        combinationSum2(nums, target - nums[idx], idx + 1, outputSoFar, ans);
+        outputSoFar.remove(outputSoFar.size() - 1);
+    }
+}
