64e87844bd01ad40dfce8852f268b42cbd3cdaa9
Manish Sharma <53977826+manish172021@users.noreply.github.com>
2026-05-27 11:05:50 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.java
@@ -0,0 +1,59 @@
+class Solution {
+
+    public boolean validTree(int n, int[][] edges) {
+        // A graph is tree only if:
+
+        // 1. No cycle
+        // 2. Fully connected
+        // 3. edges = n - 1
+
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+
+        for(int i=0; i<n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for(int[] edge : edges) {
+            adjList.get(edge[1]).add(edge[0]);
+            adjList.get(edge[0]).add(edge[1]);
+        }
+
+        // necessary condition
+        if(edges.length != n - 1) {
+            return false;
+        }
+
+        return !detectCyclyeUD(n, adjList);
+    }
+
+    private boolean detectCyclyeUD(int V, ArrayList<ArrayList<Integer>> adjList) {
+        int[] parent = new int[V];
+
+        for(int i=0; i<V; i++) {
+            parent[i] = i;
+        }
+
+        for(int u = 0; u < V; u++) {
+            for(int v : adjList.get(u)) {
+                if(u < v) {
+                    int sourceParent = findParent(u, parent);
+                    int destParent = findParent(v, parent);
+
+                    if(sourceParent == destParent) {
+                        return true;
+                    }
+
+                    parent[sourceParent] = destParent;
+                }
+            }
+        }
+        return false;
+    }
+
+    private int findParent(int node, int[] parent) {
+        if(parent[node] == node) {
+            return node;
+        }
+        return findParent(parent[node], parent);
+    }
+}
