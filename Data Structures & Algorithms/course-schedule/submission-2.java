a07804cca1d19adfeeac73f68e3a5b5bf32d58e1
Manish Sharma <53977826+manish172021@users.noreply.github.com>
2026-05-27 10:48:49 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.java
@@ -0,0 +1,65 @@
+class Solution {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+
+        for(int i=0; i<numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for(int[] prerequisite : prerequisites) {
+            // to take a, you must first complete b
+            // edge should be b -> a
+            adjList.get(prerequisite[1]).add(prerequisite[0]);
+        }
+
+        return !bfsDetectcycle(numCourses, adjList);
+    }
+
+    //   ->    
+    // 0    1
+    //   <-
+
+    // 1 -> 1 0 -> 1
+
+
+    // 0 -> 1, 2, 3
+    // 1 -> 0
+
+    public boolean bfsDetectcycle(int v, ArrayList<ArrayList<Integer>> adjList) {
+        int[] indegree = new int[v];
+
+        for(ArrayList<Integer> list : adjList) {
+            for(int val : list) {
+                indegree[val]++;
+            }
+        }
+  
+        Queue<Integer> pendingNode = new LinkedList<>();
+        
+        for(int i= 0; i<v; i++) {
+            if(indegree[i] == 0) {
+                pendingNode.add(i);
+            }
+        }
+
+
+        int count = 0;
+
+        while(!pendingNode.isEmpty()) {
+            int front = pendingNode.poll();
+            count++;
+
+            for(int neighbour : adjList.get(front)) {
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0) {
+                    pendingNode.add(neighbour);
+                }
+            }
+        }
+
+        return v != count;
+
+
+    }
+}
